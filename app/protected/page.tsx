a918b43ee2866ab7ba743ce8759aae919569bfab
norio <norio@kitamura.us>
2023-11-01 18:47:53 -0700

    update demo video URL

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
     <div className="flex h-screen bg-black">
       <div className="w-screen h-screen flex flex-col space-y-5 justify-center items-center">
         <iframe
-          src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1"
-          title="YouTube video player"
+          src="https://www.youtube.com/embed/xCSHDwZljic?autoplay=1"
+          title="YouTube video public domain content"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
           className="w-full max-w-screen-lg aspect-video"
