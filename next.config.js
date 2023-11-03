91e9f3b90c2a5c5e5aac1596c7593718ecc54fb1
norio <norio@kitamura.us>
2023-11-02 19:36:13 -0700

    update user registration logic

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/login',
+        permanent: true,
+      },
+    ];
+  }
 };
 
 module.exports = nextConfig;
